753bafb92a03dee47c75570e26f8bff0f51474a6
megumish <megumish@exploitouri.st>
2020-06-14 03:11:32 +0900

    step 2: 加減算のできるコンパイラの作成

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ![CI](https://img.shields.io/github/workflow/status/megumish/me9cc/CI?style=flat-square)
 # me9cc
 
-これは [https://www.sigbus.info/compilerbook](低レイヤを知りたい人のためのCコンパイラ作成入門) に大まかに従いつつ実装している自作Cコンパイラです。
+これは [低レイヤを知りたい人のためのCコンパイラ作成入門](https://www.sigbus.info/compilerbook) に大まかに従いつつ実装している自作Cコンパイラです。
 
 # ビルド方法
 
--- me9cc/main.c
+++ me9cc/main.c
@@ -25,9 +25,33 @@ int main(const int argc, const char *const *const argv)
     fprintf(output_file, ".intel_syntax noprefix\n");
     fprintf(output_file, ".global %s\n", argv[3]);
     fprintf(output_file, "%s:\n", argv[3]);
-    int input_int;
-    fscanf(input_file, "%d", &input_int);
-    fprintf(output_file, "  mov rax, %d\n", input_int);
+    {
+        int input_int;
+        fscanf(input_file, "%d", &input_int);
+        fprintf(output_file, "  mov rax, %d\n", input_int);
+    }
+
+    char operator;
+    while (fscanf(input_file, "%c", &operator) != -1)
+    {
+        if (operator== '+')
+        {
+            int input_int;
+            fscanf(input_file, "%d", &input_int);
+            fprintf(output_file, "  add rax, %d\n", input_int);
+            continue;
+        }
+
+        if (operator== '-')
+        {
+            int input_int;
+            fscanf(input_file, "%d", &input_int);
+            fprintf(output_file, "  sub rax, %d\n", input_int);
+            continue;
+        }
+
+        printf("invalid operator: %c", operator);
+    }
     fprintf(output_file, "  ret\n");
     return 0;
 }
